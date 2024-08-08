4f1790edc4c97c32ed0483503545c6feb47cda23
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-08-08 15:31:41 -0300

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,45 @@ import './App.css'
 function App() {
   const [registros, setRegistros] = useState([]);
   const [novoRegistro, setNovoRegistro] = useState('');
+  const [perguntaQuiz, setPerguntaQuiz] = useState('');
+  const [respostaCorreta, setRespostaCorreta] = useState(false);
 
 
   const adicionarRegistro = () => {
     setRegistros([...registros, novoRegistro]);
     setNovoRegistro('');
+  };
+
+  const alterarRegistro = (indice, novoValor) => {
+    const novosRegistros = [...registros];
+    if (indice >= 0 && indice < novosRegistros.length) {
+      if (novoValor.trim() === '') {
+        alert("O novo valor não pode estar vazio!");
+      } else {
+        novosRegistros[indice] = novoValor;
+        setRegistros(novosRegistros);
+      }
+    } else {
+      console.log("Índice inválido");
+    }
+  };
+
+  const apagarRegistro = (indice) => {
+    const novosRegistros = [...registros];
+    if (indice >= 0 && indice < novosRegistros.length) {
+      novosRegistros.splice(indice, 1);
+      setRegistros(novosRegistros);
+    } else {
+      console.log("Índice inválido");
+    }
+  };
+
+  const exibirPergunta = () => {
+    // Defina sua pergunta do quiz aqui
+    setPerguntaQuiz(`Qual destes você fez primeiro hoje? ${registros[1]}`); // Exemplo de pergunta
+
+    // Defina a resposta correta (true para certo, false para errado)
+    setRespostaCorreta(true); // Exemplo de resposta correta
   };
 
   return (
@@ -21,11 +55,30 @@ function App() {
         placeholder="Digite seu registro"
       />
       <button onClick={adicionarRegistro}>Adicionar</button>
-      <ul id="listaRegistros">
+      <ul id="listaRegistros" className='registros'>
         {registros.map((registro, index) => (
-          <li key={index}>{registro}</li>
+          <li key={index} className='registro'>
+            {registro}
+            <button onClick={() => alterarRegistro(index, prompt("Digite o novo valor:"))} >
+
+              <img src="./pincel-grande.png" alt="Editar" /></button> <button onClick={() => apagarRegistro(index)}>
+              Apagar
+            </button></li>
         ))}
       </ul>
+
+      <button onClick={exibirPergunta}>Exibir Pergunta</button>
+      {perguntaQuiz && (
+        <div>
+          <p>{perguntaQuiz}</p>
+          <button onClick={() => alert(respostaCorreta ? "Certo!" : "Errado!")}>
+            aaaa
+          </button>
+          <button onClick={() => alert(respostaCorreta ? "Errado!" : "Certo!")}>
+            bbbb
+          </button>
+        </div>)
+      }
     </>
   )
 }
