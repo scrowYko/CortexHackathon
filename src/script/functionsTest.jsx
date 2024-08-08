ebd84bb2b9b095e6057d37216134f2c48255a319
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-08-08 13:37:34 -0300

    atualizando

--- src/script/functionsTest.jsx
+++ src/script/functionsTest.jsx
@@ -1,8 +1,24 @@
-const listaRegistros = []
-
-function novoRegistro(registro){
-    listaRegistros.push(registro)
-    return listaRegistros
+function criarRegistro(registros, novoRegistro) {
+    registros.push(novoRegistro);
+    return registros;
 }
 
-export default {novoRegistro}
+function alterarRegistro(registros, indice, registroAtualizado) {
+    if (indice >= 0 && indice < registros.length) {
+        registros[indice] = registroAtualizado;
+    } else {
+        console.log("Índice inválido");
+    }
+    return registros;
+}
+
+
+function deletarRegistro(registros, indice) {
+    if (indice >= 0 && indice < registros.length) {
+        registros.splice(indice, 1);
+    } else {
+        console.log("Índice inválido");
+    }
+    return registros;
+}
+
