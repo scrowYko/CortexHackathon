b47d785df9b4c37fd92de997965c99a65cefdc85
DAVI HENRY MOREL PINTOS <davi_pintos@estudante.sesisenai.org.br>
2024-08-08 14:19:38 -0300

    implementando a funcao de adicionar registro

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
+import { React, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [registros, setRegistros] = useState([]);
+  const [novoRegistro, setNovoRegistro] = useState('');
+
+
+  const adicionarRegistro = () => {
+    setRegistros([...registros, novoRegistro]);
+    setNovoRegistro('');
+  };
 
   return (
     <>
       <h1>Registros do Dia</h1>
-    <input type="text" id="novoRegistro" placeholder="Digite seu registro"/>
-    <button onclick="adicionarRegistro()">Adicionar</button>
-    <ul id="listaRegistros"></ul>
+      <input
+        type="text"
+        value={novoRegistro}
+        onChange={(e) => setNovoRegistro(e.target.value)}
+        placeholder="Digite seu registro"
+      />
+      <button onClick={adicionarRegistro}>Adicionar</button>
+      <ul id="listaRegistros">
+        {registros.map((registro, index) => (
+          <li key={index}>{registro}</li>
+        ))}
+      </ul>
     </>
   )
 }
--- src/script/functionsTest.test.jsx
+++
@@ -1,7 +0,0 @@
-const novoRegistro = require('./functionsTest')
-
-test('Teste para ver se registros estão funcionando', () => {
-    const registro = 'Hoje fui a academia'
-    const saida = 'Hoje fui a academia'
-    expext(novoRegistro(registro).toEqual(saida))
-})
